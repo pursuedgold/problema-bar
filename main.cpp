c8baaddf225742d879f6fdbc93049d23c0b11b59
pursuedgold <filipipw@gmail.com>
2020-05-07 22:46:07 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+int main() {
+	string sexo;
+	int cerv, refr, espeto;
+	double couvert, consumo, total,ingresso ;
+	cout << "Sexo: " << endl;
+	cin >> sexo;
+	
+	cout << "Quantidade de cervejas: " << endl;	
+	cin >> cerv;
+	
+	cout << "Quantidade de refrigerantes: " << endl;
+	cin >> refr;
+	
+	
+	cout << "Quantidade de espetinhos: " << endl;
+	cin >> espeto;
+	
+	
+	
 	return 0;
 }
